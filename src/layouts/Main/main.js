9822ffeebdfbc9ccb93038134655f161006a40d0
Nikodem <nikodem.lorenz@gmail.com>
2019-07-04 14:11:59 +0200

    initial; created basic structure

---
+++ src/layouts/Main/main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+
+export default () => {
+
+   return (
+      <div className="layout">
+         <Navbar/>
+      </div>
+   )
+}
