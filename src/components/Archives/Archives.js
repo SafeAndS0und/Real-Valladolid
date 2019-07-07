5d1413526e967d0234bf4c2ef126b9082afb6375
Nikodem <nikodem.lorenz@gmail.com>
2019-07-07 21:18:31 +0200

    Archives are getting now data from graphql file transformer

--- src/components/Archives/Archives.js
+++ src/components/Archives/Archives.js
@@ -1,37 +1,71 @@
 import React from 'react'
 import ArchivesStyles from './Archives.module.scss'
+import {useStaticQuery, graphql} from 'gatsby'
 
 export default () =>{
+
+   const {allFile: {edges}} = useStaticQuery(graphql` 
+      query {
+        allFile {
+          edges {
+            node {
+              name
+              relativeDirectory
+              childMarkdownRemark {
+                frontmatter {
+                  title
+                }
+              }
+            }
+          }
+        }
+      }
+   `)
+
+   const months = edges.map(({node: {relativeDirectory}}) => relativeDirectory)
+   const uniqueMonths = Array.from(new Set(months))
 
    return (
       <aside>
 
-         <div className={ArchivesStyles.month}>
-            <h3>Lipiec</h3>
-            <p>Brak postów</p>
-         </div>
+         {uniqueMonths.map(month =>{
+            return (
+               <div className={ArchivesStyles.month}>
+                  <h3>{month}</h3>
+                  {edges.map(({node}) =>{
+                     if(node.relativeDirectory === month)
+                        return <p>{node.childMarkdownRemark.frontmatter.title}</p>
+                  })}
+               </div>
+            )
+         })}
 
-         <div className={ArchivesStyles.month}>
-            <h3>Czerwiec (2)</h3>
-            <p>Pierwszy Post</p>
-            <p>Drugi Pościk</p>
-         </div>
+         {/*<div className={ArchivesStyles.month}>*/}
+         {/*<h3>Lipiec</h3>*/}
+         {/*<p>Brak postów</p>*/}
+         {/*</div>*/}
 
-         <div className={ArchivesStyles.month}>
-            <h3>Maj (5)</h3>
-            <p>Pierwszy Post</p>
-            <p>Cos tam gdzies tam</p>
-            <p>Lorem ipsum</p>
-            <p>Drugi Pościk</p>
-            <p>Whoooops </p>
-         </div>
+         {/*<div className={ArchivesStyles.month}>*/}
+         {/*<h3>Czerwiec (2)</h3>*/}
+         {/*<p>Pierwszy Post</p>*/}
+         {/*<p>Drugi Pościk</p>*/}
+         {/*</div>*/}
 
-         <div className={ArchivesStyles.month}>
-            <h3>Kwiecień (3)</h3>
-            <p>Lorem ipsum</p>
-            <p>Pierwszy Post</p>
-            <p>Drugi Pościk</p>
-         </div>
+         {/*<div className={ArchivesStyles.month}>*/}
+         {/*<h3>Maj (5)</h3>*/}
+         {/*<p>Pierwszy Post</p>*/}
+         {/*<p>Cos tam gdzies tam</p>*/}
+         {/*<p>Lorem ipsum</p>*/}
+         {/*<p>Drugi Pościk</p>*/}
+         {/*<p>Whoooops </p>*/}
+         {/*</div>*/}
+
+         {/*<div className={ArchivesStyles.month}>*/}
+         {/*<h3>Kwiecień (3)</h3>*/}
+         {/*<p>Lorem ipsum</p>*/}
+         {/*<p>Pierwszy Post</p>*/}
+         {/*<p>Drugi Pościk</p>*/}
+         {/*</div>*/}
 
       </aside>
    )
